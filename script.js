dccc02894c5a2e0cabb534ef94be601147b6ea7a
Bruno Costa <bmbrunocosta@hotmail.com>
2026-06-04 14:23:36 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const URL_API = "https://script.google.com/macros/s/AKfycbwq0Jl-aCRFeJ2thbMpIF4USycY5M-khpw07N4x1F4kX6ayg_3AOiYA12GiiXeMCfhD2w/exec";
+const URL_API = "https://script.google.com/macros/s/AKfycbxcl4s5z6itCIPMHcxUpqrGFOBgmaNVtCtXzOXvlKxWEavQBhS6D9po-WcM5TY48VH52Q/exec";
 
 const rgEntra = document.getElementById("rgEntra");
 const rgSai = document.getElementById("rgSai");
